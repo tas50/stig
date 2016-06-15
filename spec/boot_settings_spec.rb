db8c8aac2cb7ce77a0130b8878929d392e592350
Ivan Suftin <isuftin@usgs.gov>
2016-06-15 09:46:08 -0500

    Updated some ChefSpec tests

--- spec/boot_settings_spec.rb
+++ spec/boot_settings_spec.rb
@@ -2,23 +2,29 @@ require "spec_helper"
 
 describe "stig::boot_settings" do
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
-    
-    it "creates /etc/sysconfig/init template" do
-      expect(chef_run).to create_template("/etc/sysconfig/init").with(
-        source: "etc_sysconfig_init.erb",
-        owner: "root",
-        group: "root",
-        mode: 0644,
-      )
-    end  
-    
-    it "creates /etc/audit/auditd.conf template" do
-      expect(chef_run).to create_template("/etc/audit/auditd.conf").with(
-        source: "etc_audit_auditd.conf.erb",
-        owner: "root",
-        group: "root",
-        mode: 0640,
-      )
-    end
+
+  before do
+    stub_command("grep -q 'selinux=0' /etc/grub.conf").and_return(false)
+    stub_command("grep -q 'enforcing=0' /etc/grub.conf").and_return(false)
+    stub_command("grep -q 'password' /etc/grub.conf").and_return(false)
+  end
+
+  it "creates /etc/sysconfig/init template" do
+    expect(chef_run).to create_template("/etc/sysconfig/init").with(
+      source: "etc_sysconfig_init.erb",
+      owner: "root",
+      group: "root",
+      mode: 0644
+    )
+  end
+
+  it "creates /etc/audit/auditd.conf template" do
+    expect(chef_run).to create_template("/etc/audit/auditd.conf").with(
+      source: "etc_audit_auditd.conf.erb",
+      owner: "root",
+      group: "root",
+      mode: 0640
+    )
+  end
 
 end
