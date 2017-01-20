e51330f0373bac12b6a6299b9e8ba248fa251e1d
Ivan Suftin <isuftin@usgs.gov>
2017-01-20 10:19:48 -0600

    Removed errors in chefspec

--- spec/boot_settings_spec.rb
+++ spec/boot_settings_spec.rb
@@ -7,6 +7,9 @@ describe 'stig::boot_settings' do
     stub_command("grep -q 'selinux=0' /etc/grub.conf").and_return(false)
     stub_command("grep -q 'enforcing=0' /etc/grub.conf").and_return(false)
     stub_command("grep -q 'password' /etc/grub.conf").and_return(false)
+    stub_command("grep -q 'selinux=0' /boot/grub2/grub.cfg").and_return(false)
+    stub_command("grep -q 'enforcing=0' /boot/grub2/grub.cfg").and_return(false)
+    stub_command("echo $(getenforce) | awk '{print tolower($0)}' | grep enforcing").and_return(false)
   end
 
   it 'creates /etc/sysconfig/init template' do
@@ -15,15 +18,6 @@ describe 'stig::boot_settings' do
       owner: 'root',
       group: 'root',
       mode: 0o644
-    )
-  end
-
-  it 'creates /etc/audit/auditd.conf template' do
-    expect(chef_run).to create_template('/etc/audit/auditd.conf').with(
-      source: 'etc_audit_auditd.conf.erb',
-      owner: 'root',
-      group: 'root',
-      mode: 0o640
     )
   end
 end
