3095846cfc658830a6f02257807bbede05eaa3e9
Suftin, Ivan <isuftin@usgs.gov>
2017-03-30 16:19:37 -0500

    Update tcp_wrappers test

--- test/integration/centos6/inspec/controls/tcp_wrappers_spec.rb
+++ test/integration/centos6/inspec/controls/tcp_wrappers_spec.rb
@@ -1,13 +1,4 @@
 
-
-if %w(debian ubuntu).include?(os['family'])
-  describe package('tcpd') do
-    it { should be_installed }
-  end
-end
-
-if %w(redhat fedora centos rhel).include?(os['family'])
-  describe package('tcp_wrappers') do
-    it { should be_installed }
-  end
+describe package('tcp_wrappers') do
+  it { should be_installed }
 end
